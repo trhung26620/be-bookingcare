7724105b2c0c754e974eb111518d2c91a7aa74aa
trhung26620 <trhung26620@gmail.com>
2022-12-24 20:48:37 +0700

    fix bug ViewEngine not found

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 require('dotenv').config();
 import initWebRoute from './route/web'
-import configViewEngine from './configs/viewEngine';
+import configViewEngine from './configs/ViewEngine';
 import initAPIRoute from './route/api';
 import connectDB from './config/connectDB';
 import cors from 'cors';
